6b0339fe4d42d27349d09d2e2df697e6b01e53bc
IPa Chen <ipapamagic@gmail.com>
2015-07-17 17:39:17 +0800

    swift support

--- UpdateCodeStrings.sh
+++ UpdateCodeStrings.sh
@@ -28,20 +28,23 @@ else
 	fullPath="$baseDir"
 
 	clear
-
 	echo "************************************************************************************"
-	echo "************************************************************************************"
-	echo "grepping NSLocalizedString(,) occurrences."
+#	echo "grepping NSLocalizedString(,) occurrences."
 	echo "Start: $fullPath"
 
 	# change directory into fullPath
+
 	cd "$fullPath"
-	pwd
 
 	find . -type f -name "*.m*" -print > listOfFiles.txt
    # cat ~/ok.txt | egrep -i ‘NSLocalizedString\(\s*@\s*”.*?”\s*,\s*@“.*?”\)’
 #	cat listOfFiles.txt |tr '\n' '\0' |xargs -0 grep -o "NSLocalizedString(@\"[[:graph:] ]*\" *, *@\"[[:graph:] ]*\")" > output.txt
-    cat listOfFiles.txt |tr '\n' '\0' |xargs -0 egrep -o 'NSLocalizedString\(\s*@".*?"\s*,\s*@".*?"\)' > output.txt
+
+    cat listOfFiles.txt |tr '\n' '\0' |xargs -0 egrep -o 'NSLocalizedString\(\s*@\".+?\"\s*,\s*@\".*?\"\s*\)' > output.txt
+    echo "*****Swift*****" >> output.txt
+	find . -type f -name "*.swift" -print >> listOfFiles.txt
+    cat listOfFiles.txt |tr '\n' '\0' |xargs -0 egrep -o 'NSLocalizedString\(\s*\".+?\"\s*,\s*tableName:.+?,\s*bundle:.+?,\s*value:\s*\".*?\"\s*,\s*comment:\s*\".*?\"\s*\)' >> output.txt
+
 	# Next, get all locale strings folders.  If Localizable.strings found, append to existing file; otherwise, create a new one.
 
 	for localeStringsDir in `find . -name "*$localeDirExt" -print`
@@ -50,12 +53,15 @@ else
 		echo "Found Locale File.\n"
 		echo "strings dir: $localeStringsDir"
 
+        pwd
 		foundDir=$(echo $localeStringsDir | grep -o "Base.lproj")
 		length=$(echo ${#foundDir})
 
-		# Make sure we aren't touching Base.lproj directory
-		if [ $length -eq 0 ]; then
+#	# Make sure we aren't touching Base.lproj directory
+#		if [ $length -eq 0 ]; then
 			# change directory into localeStringsDir
+#            cd "$EXCUTE_PATH"
+            cd "$fullPath"
 			cd "$localeStringsDir"
 
 			echo "cd down to:"
@@ -71,49 +77,77 @@ else
 
 			# For each Localizable.strings file, loop through output.txt and parse out the key/value pairs for the localized strings.
 			# If the key already exists in the file, then skip; otherwise, append the key/value (in proper format) to the end of the file.
-
+            SWIFT_CODE="OFF"
 			while read LINE
 			do
 #				foundLocalizedString=$(echo "$LINE" | grep -o "NSLocalizedString(@\"[[:alnum:]]*\", @\"[a-zA-Z0-9 !@#\$%\^&\*()\.,-\+']*\")")
 #				foundKey=$(echo "$foundLocalizedString" | grep -o "(@\"[[:alnum:]]*\"")
-                foundLocalizedString=$(echo "$LINE" | egrep -o 'NSLocalizedString\(\s*@".*?"\s*,\s*@".*?"\)')
-				foundKey=$(echo "$foundLocalizedString" | egrep -o '\(@".*?"')
+                if [ "$LINE" = "*****Swift*****" ]; then
+                    SWIFT_CODE="ON"
+                    continue
+                fi
+                if [[ "$SWIFT_CODE" = "OFF" ]]; then
+#                    echo "objc:${LINE}"
+                    foundLocalizedString=$(echo "$LINE" | egrep -o 'NSLocalizedString\(\s*@".+?"\s*,\s*@".*?"\s*\)')
+                    foundKey=$(echo "$foundLocalizedString" | egrep -o '\(@".*?"')
+                    keyStart="\""
+                    finalKey=$(echo "$foundKey" | grep -o "$keyStart.*")
 
-				keyStart="\""
-				finalKey=$(echo "$foundKey" | grep -o "$keyStart.*")
+                    $(grep -q "$finalKey" $stringsFile)
 
-				$(grep -q "$finalKey" $stringsFile)
+                    if [ $? -eq 1 ]; then
+                        echo "****** key is New: $finalKey"
 
-				if [ $? -eq 1 ]; then
-					echo "****** key is New: $finalKey"
+                        foundComment=$(echo "$foundLocalizedString" | egrep -o ",\s*@\".*?\"\)")
+                        commentStart="\""
+                        intermediateComment=$(echo "$foundComment" | egrep -o "$commentStart.*")
 
-					foundComment=$(echo "$foundLocalizedString" | grep -o ",\s*@\".*\")")
-					commentStart="\""
-					intermediateComment=$(echo "$foundComment" | grep -o "$commentStart.*")
+                        finalComment=$(echo "$intermediateComment" | sed "s/)//")
 
-					finalComment=$(echo "$intermediateComment" | sed "s/)//")
+                        echo "/* $finalComment */" >> $stringsFile
+                        echo -e "$finalKey = $finalComment;\n" >> $stringsFile
+                    else
+                        echo "key Exists: $finalKey"
+                    fi
+                else
+                    foundLocalizedString=$(echo "$LINE" | egrep -o 'NSLocalizedString\(\s*\".+?\"\s*,\s*tableName:.+?,\s*bundle:.+?,\s*value:\s*\".*?\"\s*,\s*comment:\s*\".*?\"\s*\)')
+                    foundKey=$( echo "$foundLocalizedString" | egrep -o '\(\s*\".+?\"' )
+                    keyStart="\""
+                    finalKey=$(echo "$foundKey" | grep -o "$keyStart.*")
 
-					echo "/* $finalComment */" >> $stringsFile
-					echo "$finalKey = $finalComment;\n" >> $stringsFile
-				else
-					echo "key Exists: $finalKey"
-				fi
+                    $(grep -q "$finalKey" $stringsFile)
 
-			done < "../output.txt"
+                    if [ $? -eq 1 ]; then
+                        echo "****** key is New: $finalKey"
+
+                        foundComment=$(echo "$foundLocalizedString" | egrep -o "comment:\s*\".*?\"\s*)")
+                        echo "comment: $foundComment"
+                        commentStart="\""
+                        intermediateComment=$(echo "$foundComment" | egrep -o "$commentStart.*")
+
+                        finalComment=$(echo "$intermediateComment" | sed "s/)//")
+
+                        echo "/* $finalComment */" >> $stringsFile
+                        echo -e "$finalKey = $finalComment;\n" >> $stringsFile
+                    else
+                        echo "key Exists: $finalKey"
+                    fi
+                fi
+
+			done < "$fullPath/output.txt"
 
 			# change directory back to baseDir
-			cd ..
+
 
 			echo "cd back up ..:"
 			pwd
 
-			echo "*!*!*!*!!!!"
-
-		else
-			echo "Ignoring: $localeStringsDir"
-		fi
+#	else
+#			echo "Ignoring: $localeStringsDir"
+#		fi
 	done
 
 	echo "\nDone."
 fi
+
 
--- UpdateStoryboardStrings.sh
+++ UpdateStoryboardStrings.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-# cls.sh - script to  Create Locale Strings auto
-#  
+# Update storyboard string files
 #
-#  Created by Gregory Hill on 2/27/13.
-#  Copyright (c) 2013 Gregory Hill. All rights reserved.
+# by 2013 André Pinto andredsp@gmail.com
+# based on http://forums.macrumors.com/showthread.php?t=1467446
 
 #update storyboard string   ./UpdateStoryboardString ../Express
 #update xib string          ./UpdateStoryboardString ../Express xib
@@ -14,99 +12,82 @@ clear
 
 baseDir=$1
 Ext=$2
-baseDirLength=$(echo ${#baseDir})
 
-if [ $baseDirLength -eq 0 ]; then
-	echo "cls.sh <rootPath>"
-else
-    storyboardExt=".storyboard"
-    size=${#Ext}
-    if [ $size -ne 0 ]; then
-        storyboardExt=".$Ext"
-    fi
-	stringsExt=".strings"
-	newStringsExt=".strings.new"
-	oldStringsExt=".strings.old"
-	localeDirExt=".lproj"
-
-	isNewStringsFile=false
-
-	echo "********************************************"
-	echo "Starting 'Create Locale Strings'"
-	echo "Moving to dir: $baseDir"
-    echo "Looking ext: $Ext"
-	cd $baseDir
-
-	# Find storyboard file full path inside project folder
-	for storyboardPath in `find . -name "*$storyboardExt" -print`
-	do
-   
-		# Get Base strings file full path
-		baseStringsPath=$(echo "$storyboardPath" | sed "s/$storyboardExt/$stringsExt/")
-
-		echo "\nstoryboardPath: $storyboardPath"
-		echo "\nbaseStringsPath: $baseStringsPath\n"
-
-		find . -type f -name "$baseStringsPath" -print > listOfFiles.txt
-
-		if [ -f $baseStringsPath ]; then
-			echo "File $baseStringsPath exists"
-
-			isNewStringsFile=false
-		else
-			echo "$baseStringsPath file doesn't exist; create"
-
-			ibtool --export-strings-file $baseStringsPath $storyboardPath
-
-			isNewStringsFile=true
-		fi
-
-		# Create strings file only when storyboard file newer
-		if $isNewStringsFile || find $storyboardPath -prune -newer $baseStringsPath -print | grep -q .; then
-			echo "$storyboardPath is modified; update $baseStringsPath"
-
-			# Get storyboard file name and folder
-			storyboardFile=$(basename "$storyboardPath")
-			storyboardDir=$(dirname "$storyboardPath")
-
-			# Get New Base strings file full path and strings file name
-			newBaseStringsPath=$(echo "$storyboardPath" | sed "s/$storyboardExt/$newStringsExt/")
-			stringsFile=$(basename "$baseStringsPath")
-            echo "excute ibtool export"
-			ibtool --export-strings-file $newBaseStringsPath $storyboardPath
-
-			iconv -f UTF-16 -t UTF-8 $newBaseStringsPath > $baseStringsPath
-
-			rm $newBaseStringsPath
-
-			# Get all locale strings folder
-			for localeStringsDir in `find . -name "*$localeDirExt" -print`
-			do
-				echo "******* \nlocaleStringsDir: $localeStringsDir"
-
-				# Skip Base strings folder
-				if [ $localeStringsDir != $storyboardDir ]; then
-					localeStringsPath=$localeStringsDir/$stringsFile
-
-					echo "localeStringsPath: $localeStringsPath\n"
-
-					# Just copy base strings file on first time
-					if [ ! -e $localeStringsPath ]; then
-						cp $baseStringsPath $localeStringsPath
-					else
-						oldLocaleStringsPath=$(echo "$localeStringsPath" | sed "s/$stringsExt/$oldStringsExt/")
-						cp $localeStringsPath $oldLocaleStringsPath
-
-						# Merge baseStringsPath to localeStringsPath
-						awk 'NR == FNR && /^\/\*/ {x=$0; getline; a[x]=$0; next} /^\/\*/ {x=$0; print; getline; $0=a[x]?a[x]:$0; printf $0"\n\n"}' $oldLocaleStringsPath $baseStringsPath > $localeStringsPath
-
-						rm $oldLocaleStringsPath
-					fi
-				fi
-			done
-        else
-			echo "$storyboardPath file not modified."
-		fi
-	done
+storyboardExt=".storyboard"
+size=${#Ext}
+if [ $size -ne 0 ]; then
+    storyboardExt=".$Ext"
 fi
+stringsExt=".strings"
+newStringsExt=".strings.new"
+oldStringsExt=".strings.old"
+localeDirExt=".lproj"
+baselprojName="Base.lproj"
+cd $baseDir
+# Find Base internationalization folders
+find . -name "$baselprojName" | while read baselprojPath
+do
+    # Get Base project dir
+    baselprojDir=$(dirname "$baselprojPath")
+
+    # Find storyboard file full path inside base project folder
+    find "$baselprojPath" -name "*$storyboardExt" | while read storyboardPath
+    do
+    # Get Base strings file full path
+        baseStringsPath=$(echo "$storyboardPath" | sed "s/$storyboardExt/$stringsExt/")
+
+    # Get storyboard file name and folder
+        storyboardFile=$(basename "$storyboardPath")
+        storyboardDir=$(dirname "$storyboardPath")
+
+    #Get the full path of the storyboard and the temporary string files. This is the fix for ibtool error
+        storyboardFullPath=$(echo $(echo `pwd`)$(echo "$storyboardPath" | cut  -c2-))
+        newBaseStringsFullPath=$(echo `pwd`"/Main.strings.new")
+
+        echo "Full path of the storyboard $storyboardFullPath"
+        echo "Full path of the temporary string files $newBaseStringsFullPath"
+
+    # Get New Base strings file full path and strings file name
+        stringsFile=$(basename "$baseStringsPath")
+
+    # Create strings file only when storyboard file newer
+        newer=$(find "$storyboardPath" -prune -newer "$baseStringsPath")
+        [ -f "$baseStringsPath" -a -z "$newer" ] && {
+        echo "$storyboardFile file not modified."
+            continue
+        }
+
+        echo "Creating default $stringsFile for $storyboardFile..."
+
+        ibtool --export-strings-file "$newBaseStringsFullPath" "$storyboardFullPath"
+        iconv -f UTF-16 -t UTF-8 "$newBaseStringsFullPath" > "$baseStringsPath"
+        rm "$newBaseStringsFullPath"
+
+    # Get all locale strings folder with same parent as Base
+        ls -d "$baselprojDir/"*"$localeDirExt" | while read localeStringsDir
+        do
+        # Skip Base strings folder
+            [ "$localeStringsDir" = "$storyboardDir" ] && continue
+
+            localeDir=$(basename "$localeStringsDir")
+            localeStringsPath="$localeStringsDir/$stringsFile"
+
+        # Just copy base strings file on first time
+            if [ ! -e "$localeStringsPath" ]; then
+                echo "Copying default $stringsFile for $localeDir..."
+                cp "$baseStringsPath" "$localeStringsPath"
+            else
+                echo "Merging $stringsFile changes for $localeDir..."
+                oldLocaleStringsPath=$(echo "$localeStringsPath" | sed "s/$stringsExt/$oldStringsExt/")
+                cp "$localeStringsPath" "$oldLocaleStringsPath"
+
+                # Merge baseStringsPath to localeStringsPath
+                awk 'NR == FNR && /^\/\*/ {x=$0; getline; a[x]=$0; next} /^\/\*/ {x=$0; print; getline; $0=a[x]?a[x]:$0; printf $0"\n\n"}' $oldLocaleStringsPath $baseStringsPath > $localeStringsPath
+                rm "$oldLocaleStringsPath"
+            fi
+        done
+    done
+done
+
+
 
